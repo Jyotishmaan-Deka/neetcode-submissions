e76db3a1d2705a2f298407b3b761aa563a111b00
JyotishmaanD <47564721+Jyotishmaan-Deka@users.noreply.github.com>
2026-06-14 21:09:07 +0530

    Add: concatenation-of-array - submission-4

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-4.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] numsNew = new int[nums.length*2];
+        for(int i = 0; i<nums.length; i++){
+            numsNew[i] = nums[i];
+            numsNew[i+nums.length] = nums[i];
+        }
+
+        return numsNew;
+    }
+}
